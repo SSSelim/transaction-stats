85506ac6761d6339598c0992ad14f9382c98b2ca
selimssevgi <selimssevgi@gmail.com>
2018-04-20 14:55:15 +0300

    initialize project

---
+++ src/main/java/com/selimssevgi/trxstats/TrxStatsApplication.java
@@ -0,0 +1,12 @@
+package com.selimssevgi.trxstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrxStatsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TrxStatsApplication.class, args);
+  }
+}
---
+++ src/test/java/com/selimssevgi/trxstats/TrxStatsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.selimssevgi.trxstats;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrxStatsApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
