f8806f8a00c68951913be8236e9e7620782ff966
selimssevgi <selimssevgi@gmail.com>
2018-04-21 23:02:09 +0300

    change collection from list to sortedSet

--- src/main/java/com/selimssevgi/trxstats/repository/TransactionRepositoryImpl.java
+++ src/main/java/com/selimssevgi/trxstats/repository/TransactionRepositoryImpl.java
@@ -4,18 +4,29 @@ import com.selimssevgi.trxstats.domain.Transaction;
 import com.selimssevgi.trxstats.domain.shared.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 /**
  * Transaction repository implementation using in-memory data structure.
+ * The implementation is not thread-safe.
  */
 @Service
 public class TransactionRepositoryImpl implements TransactionRepository {
 
-  private List<Transaction> transactions = new ArrayList<>();
+  /**
+   * Keeping the newest transaction in the beginning of collection.
+   * Helps to reach the one in the transaction limit faster.
+   */
+  private SortedSet<Transaction> transactions = new TreeSet<>(comparingByTime().reversed());
+
+  private Comparator<Transaction> comparingByTime() {
+    return Comparator.comparingLong(trx -> trx.time().value());
+  }
 
   @Override
   public void save(Transaction transaction) {
@@ -26,6 +37,7 @@ public class TransactionRepositoryImpl implements TransactionRepository {
   @Override
   public List<Transaction> findAllBySpecification(Specification<Transaction> specification) {
       return transactions.stream()
+              // takeWhile would help use here. SortedSet would be more meaningful.
               .filter(specification::isSatisfiedBy)
               .collect(Collectors.toList()); // returns empty list if none found
   }
