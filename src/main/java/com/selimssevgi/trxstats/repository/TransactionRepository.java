cb55aecf4cae5df9767aa03a3efca5df22c11669
selimssevgi <selimssevgi@gmail.com>
2018-04-23 11:38:39 +0300

    add getStatistics method to TransactionRepository interface

--- src/main/java/com/selimssevgi/trxstats/repository/TransactionRepository.java
+++ src/main/java/com/selimssevgi/trxstats/repository/TransactionRepository.java
@@ -1,7 +1,9 @@
 package com.selimssevgi.trxstats.repository;
 
 import com.selimssevgi.trxstats.domain.Transaction;
+import com.selimssevgi.trxstats.domain.shared.Statistics;
 
 public interface TransactionRepository extends Repository<Transaction> {
+  Statistics getStatistics();
 }
 
